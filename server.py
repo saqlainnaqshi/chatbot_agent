ca7651549b07f9f6a7c7a09c168c1e5ac613c2ea
Saqlain Naqshi <saqlainnaqshi@icloud.com>
2024-07-19 15:30:23 +0530

    fixed port for server

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from chatbot import get_response
 from vector_store import set_vector_store
 import requests
 import json
+import os
 
 app = Flask(__name__)
 
@@ -63,4 +64,5 @@ def create_vector_store():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7001)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
